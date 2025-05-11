8853f4eb46386e0b552ba95001f1c7e65fcaa6b0
giokovacevic <kovacevico012@gmail.com>
2025-05-11 02:18:12 +0200

    deleted wrong files

--- client_old/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set JAVA_FX_PATH=C:\javafx-sdk-21.0.5\lib
-set PROJECT_PATH=C:\Users\Ogi\Documents\projects\java-chess\client
-
-javac --module-path %JAVA_FX_PATH% --add-modules javafx.controls,javafx.fxml -d %PROJECT_PATH%\out %PROJECT_PATH%\src\main\Client.java
-java --module-path %JAVA_FX_PATH% --add-modules javafx.controls,javafx.fxml -cp %PROJECT_PATH%\out main.Client
-pause
